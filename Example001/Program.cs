8ef720e9b8fae52c37bc1ae0e6c3f83ffcd2b1bf
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-04 23:36:13 +0300

    midle

---
+++ Example001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberA = Console.ReadLine();
+            
+            string numberB = Console.ReadLine();
+            int Min;
+            int Max;
+            int result;
+            if (numberA > numberB);
+            {
+              result = numberA;
+            }
+
+            else
+            {
+                result = numberB; 
+            }
+          
+        }
+    }
+}
