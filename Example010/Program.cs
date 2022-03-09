bad8201bfe23409b47e9b1edf1fd924de695a40f
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-10 00:48:47 +0300

    cube

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example010
+{
+    class Program
+    {
+        static void Main()
+        {
+            int cubnum;
+            Console.WriteLine("Таблица кубов!\n\nВведите число");
+            cubnum = Int32.Parse(Console.ReadLine());
+            for (int i=1; i<=cubnum;i++)
+            Console.WriteLine(i*i*i);
+        }  
+  
+    }
+}
