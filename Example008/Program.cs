e535c08161ee2b5a5ec142ca702923befc2c572b
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-09 23:34:09 +0300

    palindrom

---
+++ Example008/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+    class Palindr
+     {
+    static void Main()
+        {
+        string s;
+            Console.WriteLine("Анализ палиндромов\n\nВведите строку:");
+          s = Console.ReadLine();
+           if (Palindrom(s))
+       Console.WriteLine("Эта строка - палиндром");
+       else Console.WriteLine("Эта строка - не палиндром");
+
+        }
+        
+        public static bool Palindrom (string s)
+        {
+                for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+                    if (s[i] != s[j])
+                        return false;
+                    return true;                    
+        }
+     
+    
+    }
