df1ccdce614b7dc29d04fa53a14bf3ab4c880439
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-03-20 12:42:37 +0300

    added max

---
+++ Example002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число:");
+int numberA = Int32.Parse(Console.ReadLine());
+int Min = numberA;
+Console.WriteLine("Введите второе число:");
+int numberB = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int numberC = Int32.Parse(Console.ReadLine());
+int Max = numberA;
+
+if (Max <= numberB)
+{
+    Max = numberB;
+}
+else 
+if (Max < numberC)
+{
+    Max = numberC;
+}
+           
+Console.Write("Максимальное число = "); 
+ Console.WriteLine(Max);
+            
+
+            
