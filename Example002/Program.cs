de861b367c13b8130fba9ad9593c846fda78c76e
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-25 22:16:38 +0300

    exelent

---
+++ Example002/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Example002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int numberA = Int32.Parse(Console.ReadLine());
+            int Max = numberA;
+            Console.WriteLine("Введите второе число:");
+            int numberB = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите второе число:");
+            int numberC = Int32.Parse(Console.ReadLine());
+            
+            //int result1=0;
+
+            if (Max < numberB)
+            {
+                Max = numberB;
+            }  
+                      
+            if (Max < numberC)
+            {
+                Max = numberC;
+            }
+            
+            
+            Console.Write("Болешее число = "); 
+            Console.WriteLine(Max);
+            
+        }
+    }
+}
