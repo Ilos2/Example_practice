a27bef1b90671ccb5ff9576db09e297b90ca7a8e
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-27 19:28:30 +0300

    excellent

---
+++ Example004/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //int numbers(int num)
+        {
+            int num;
+            Console.WriteLine("Введите число: ");
+            num = Int32.Parse(Console.ReadLine());
+            int N = 1; //произведение 
+            int i = 1;  //счётчик
+
+            while (N < num - 1)
+            {
+                N = i * 2;
+                i++;
+                Console.Write(N);
+                Console.Write(", ");
+                
+            }
+            
+        }
+    }
+}
