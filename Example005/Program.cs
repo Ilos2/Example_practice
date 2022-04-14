f4988bedf52afe50b1c58a4f25a91c487f6282c6
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-04-14 22:52:26 +0300

    excellent

---
+++ Example005/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Example005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            //string num = Console.ReadLine();
+      
+            //var center = (num.Length%10) +1;
+
+            string s;
+            string result;
+            s = Console.ReadLine();
+            if ((s.Length) % 2 == 0)
+            {
+                result = s.Substring((((s.Length) / 2)-1), 2);
+            }
+            else
+            {
+                result = s.Substring(((s.Length) / 2), 1);
+            }
+ 
+            Console.WriteLine(result);
+            
+                           
+        }
+    }
+}
