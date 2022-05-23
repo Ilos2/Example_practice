1f258f90b34d5a388f7c7f7f3d550ca4d97a8147
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-05-23 13:30:12 +0300

    Ex2_Task 15

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//int week=0;
+
+Console.WriteLine("Enter day: ");
+int day = Int32.Parse(Console.ReadLine());
+
+if (day<=5)
+    {
+        Console.WriteLine("working day");
+    }
+else if (day>=6 & day<=7)
+    {
+        Console.WriteLine("day off");
+    }
