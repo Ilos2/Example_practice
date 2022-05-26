b210021561b3de4562a77949dd4f4e86f4e9c815
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-05-26 19:12:24 +0300

    Task 13: Write the third number

---
+++ Example006/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            char[] arr;
+            string n = Console.ReadLine();
+            arr = n.ToCharArray();
+             
+            
+            //Console.WriteLine(arr.Length);
+            if (arr.Length > 2)
+            {
+            Console.WriteLine(arr[2]);
+            }
+            else 
+            Console.WriteLine("Третьего числа нет.");
+        }
+    }
+}
+
+
