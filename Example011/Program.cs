5a3990d6424ca1d60039b35662aa4dc2035082e6
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-05-28 17:01:23 +0300

    Task 1: Write a cycle, natural degree

---
+++ Example011/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int NatDegree(int a, int b)
+{
+    int result = a;
+    for (int i=0; i <= (b-2); i++)
+    {
+            result = result * a; 
+    }
+    return result;
+}
+Console.WriteLine("Введите число A:");
+int A = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int B = Int32.Parse(Console.ReadLine());
+int res = NatDegree(A,B);
+Console.WriteLine(res);
