6bded5574c21d4c149309b610136a8ad341952e4
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-05-28 19:32:36 +0300

    wtf

---
+++ Example012/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+int SumNum(string a)
+{
+    //Console.WriteLine(a[2]);
+    int result=0;
+    //int[] b = new int [a.Length];
+    for (int i=0; i < a.Length ; i++)
+    {
+        
+        if (a[i]>0)
+        {
+            result += Convert.ToInt32(a[i]); 
+            Console.WriteLine(a[i]);
+            //Console.WriteLine(b[i]);
+        }
+    }
+    
+    
+    return result;
+    
+}
+Console.WriteLine("Введите число: ");
+string A = Console.ReadLine();
+
+int res = SumNum(A);
+Console.WriteLine(res);
