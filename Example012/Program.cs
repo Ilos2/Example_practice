7f74add2b286a4380ba6d00f9de736031af67645
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-05-29 10:07:41 +0300

    Task 2: the sum of the digits in a number

--- Example012/Program.cs
+++ Example012/Program.cs
@@ -1,26 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int SumNum(string a)
+int NatDegree(string a)
 {
     //Console.WriteLine(a[2]);
-    int result=0;
-    //int[] b = new int [a.Length];
-    for (int i=0; i < a.Length ; i++)
+    int b = Convert.ToInt32(a);
+    int result = 0;
+    int m=0;
+    for (int i=0; i < a.Length; i++)
     {
-        
-        if (a[i]>0)
-        {
-            result += Convert.ToInt32(a[i]); 
-            Console.WriteLine(a[i]);
-            //Console.WriteLine(b[i]);
-        }
+        m = b % 10;
+        b = b / 10;
+        result +=m;
     }
-    
-    
     return result;
-    
 }
 Console.WriteLine("Введите число: ");
 string A = Console.ReadLine();
 
-int res = SumNum(A);
+int res = NatDegree(A);
 Console.WriteLine(res);
