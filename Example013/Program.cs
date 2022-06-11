fe64e44cc0cfbdeb2f11479d91fae3a869c6eada
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-06-11 14:50:14 +0300

    Task 3: Write a program that sets an array of 8 elements and displays them on the screen.

---
+++ Example013/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[8];
+string resalt = string.Empty;
+for (int i=0; i <= array.Length - 1; i++)
+{
+    
+    array[i] = new Random().Next(1,21);
+    resalt = array[i] + ", " + resalt;
+   
+    
+}
+string resalt2 = resalt.Substring(0, resalt.Length - 2); 
+Console.WriteLine($"Случайный массив из 8ми элементров: [{resalt2}]");
+
