e55538a84deb1526821f50c55b10f53b50b54d99
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-06-11 15:43:32 +0300

    Lesson 5. Task 1: Define an array filled with random positive three-digit numbers. Write a program to show the number of even numbers in an array

---
+++ Example014/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[4];
+string resalt = string.Empty;
+int chet = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    
+    array[i] = new Random().Next(100,999);
+    if(array[i] % 2==0)
+    {
+        chet++;
+    }
+    resalt = array[i] + ", " + resalt;
+   
+    
+}
+string resalt2 = resalt.Substring(0, resalt.Length - 2); 
+Console.WriteLine($"В массиве [{resalt2}]: чётных чисел {chet}");
