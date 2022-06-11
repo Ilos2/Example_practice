2c6f9b2af64f8f97788ef5e29391bb4639e9bfba
Илляшевич Олег Сергеевич <illyashevichos@corp.clmedical.ru>
2022-06-11 15:53:34 +0300

    Lesson 5. Task 2: Specify a one-dimensional array filled with random numbers. Find the sum of elements in odd positions.

---
+++ Example015/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[4];
+string resalt = string.Empty;
+int sum = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    
+    array[i] = new Random().Next(-99,99);
+    if(array[i] % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+    resalt = array[i] + ", " + resalt;
+   
+    
+}
+string resalt2 = resalt.Substring(0, resalt.Length - 2); 
+Console.WriteLine($"В массиве [{resalt2}]: Сумма нечётных чисел {sum}");
